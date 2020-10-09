b9d31620045c78d02f5fc1e0369de9556f4d9c87
Miro Voellmy <miro.voellmy@esa.int>
2020-10-09 17:47:48 +0200

    Covert tabs to space

--- include/locomotion_mode/stop_mode.hpp
+++ include/locomotion_mode/stop_mode.hpp
@@ -3,14 +3,16 @@
 
 #include "locomotion_mode/locomotion_mode.hpp"
 
+namespace locomotion_mode {
 
-class StopMode : public LocomotionMode
-{
-public:
-  StopMode(rclcpp::NodeOptions options, std::string node_name);
+  class StopMode : public LocomotionMode
+  {
+  public:
+    StopMode(rclcpp::NodeOptions options, std::string node_name);
 
-private:
-  void rover_velocities_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
-};
+  private:
+    void rover_velocities_callback(const geometry_msgs::msg::Twist::SharedPtr msg);
+  };
+}
 
 #endif
